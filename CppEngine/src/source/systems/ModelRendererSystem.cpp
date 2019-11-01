ce6bc39a372784415d609b1e3ac8ad0dee49e6ef
Danielwbolson <danielwbolson@gmail.com>
2019-10-31 19:22:20 -0500

    ...Except specular does need an alpha component

--- CppEngine/src/source/systems/ModelRendererSystem.cpp
+++ CppEngine/src/source/systems/ModelRendererSystem.cpp
@@ -135,7 +135,7 @@ void ModelRendererSystem::Setup() {
     // specular color
     glGenTextures(1, &(gBuffer.specular));
     glBindTexture(GL_TEXTURE_2D, gBuffer.specular);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, screenWidth, screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, screenWidth, screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, GL_TEXTURE_2D, gBuffer.specular, 0);
